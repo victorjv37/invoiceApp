84cd0cb3860397b577b9a0e1c515e65410df57fc
Curro Jimenez <curro.jimenez.dev@gmail.com>
2023-02-07 17:17:49 +0100

    Capaz de borrar una factura desde la vista de todas las facturas

--- routes/delete.js
+++ routes/delete.js
@@ -1,23 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const invoiceModel = require('../models/invoice.js');
+const invoiceModel = require("../models/invoice.js");
+var pg = require("pg");
 
+router.delete("/:invoiceNumber", (request, response) => {
+  console.log(request.params.invoiceNumber);
+  const id = request.params.invoiceNumber;
 
-router.delete('/:invoiceNumber',(request, response )=>{
-
-invoiceModel.deleteOne({
-         _id : request.params.invoiceNumber
-    },(err)=>{
-        if(err){
-            console.log('ERROR: '+ err );
-            response.status(500).json({message:'no se pudo borrar la factura'});
-           } else{
-            console.log('la factura se ha borrado correctamente');
-            response.status(200).json({message:'la factura se ha borrado correctamente'});
-           }
-    });
-
-    
+  var conString =
+    "postgres://tbsifhep:vc-HK00-0KSl1FysW9tMUe7jehXh0Qx4@rogue.db.elephantsql.com/tbsifhep"; //Can be found in the Details page
+  var client = new pg.Client(conString);
+  client.connect(function (err) {
+    if (err) {
+      return console.error("could not connect to postgres", err);
+    }
+    client.query(
+      `DELETE FROM invoice WHERE id = ${id};`,
+      function (err, result) {
+        if (err) {
+          return console.error("error running query", err);
+        }
+        console.log("*");
+        console.log(result.rows);
+        response.status(200).json(result.rows);
+        client.end();
+      }
+    );
+  });
 });
 
 module.exports = router;
