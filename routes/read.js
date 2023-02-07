7cfd6507331639fa2ec32aa386816a35f8d8bef6
Curro Jimenez <curro.jimenez.dev@gmail.com>
2023-02-07 17:44:47 +0100

    Capaz de leer una factura determinada

--- routes/read.js
+++ routes/read.js
@@ -4,9 +4,6 @@ const invoiceModel = require("../models/invoice.js");
 var pg = require("pg");
 
 router.get("/all", (request, response) => {
-  //or native libpq bindings
-  //var pg = require('pg').native
-
   var conString =
     "postgres://tbsifhep:vc-HK00-0KSl1FysW9tMUe7jehXh0Qx4@rogue.db.elephantsql.com/tbsifhep"; //Can be found in the Details page
   var client = new pg.Client(conString);
@@ -24,35 +21,44 @@ router.get("/all", (request, response) => {
       client.end();
     });
   });
-
-  //   invoiceModel.find((err, docs) => {
-  //     if (err) {
-  //       console.log("ERROR: " + err);
-  //       response.status(500).json({ message: "No se pudo leer la información" });
-  //     } else {
-  //       console.log("Se ha leido la información correctamente");
-  //       response.status(200).json(docs);
-  //     }
-  //   });
 });
 
 router.get("/:invoiceNumber", (request, response) => {
-  invoiceModel.findOne(
-    {
-      _id: request.params.invoiceNumber,
-    },
-    (err, doc) => {
-      if (err) {
-        console.log("ERROR: " + err);
-        response
-          .status(500)
-          .json({ message: "No se pudo encontrar la factura" });
-      } else {
-        console.log("Se ha encontrado la factura");
-        response.status(200).json(doc);
-      }
+  console.log(request.params.invoiceNumber);
+  const id = Number(request.params.invoiceNumber);
+
+  var conString =
+    "postgres://tbsifhep:vc-HK00-0KSl1FysW9tMUe7jehXh0Qx4@rogue.db.elephantsql.com/tbsifhep"; //Can be found in the Details page
+  var client = new pg.Client(conString);
+  let invoice = {};
+  client.connect(function (err) {
+    if (err) {
+      return console.error("could not connect to postgres", err);
     }
-  );
+    client.query(
+      `SELECT * FROM invoice WHERE id = ${id};`,
+      function (err, result) {
+        if (err) {
+          return console.error("error running query", err);
+        }
+        console.log("*");
+        console.log(result.rows);
+        invoice = result.rows[0];
+        client.query(
+          `SELECT * FROM item WHERE invoice_id = ${id};`,
+          function (err, result) {
+            if (err) {
+              return console.error("error running query", err);
+            }
+            invoice.items = result.rows;
+            console.log("\\", invoice);
+            response.status(200).json(invoice);
+            client.end();
+          }
+        );
+      }
+    );
+  });
 });
 
 module.exports = router;
