ce7fede75f80c5e9e1beb5b311a36ed8c56e4da2
Curro Jimenez <curro.jimenez.dev@gmail.com>
2023-02-07 19:58:24 +0100

    Actualizando facturas con SQL

--- routes/update.js
+++ routes/update.js
@@ -1,36 +1,37 @@
 const express = require("express");
 const router = express.Router();
 const invoiceModel = require("../models/invoice.js");
+var pg = require("pg");
 
 router.patch("/:invoiceId", (request, response) => {
-  invoiceModel.updateOne(
-    {
-      _id: request.params.invoiceId,
-    },
-    {
-      sellername: request.body.sellername,
-      selleraddress: request.body.selleraddress,
-      customername: request.body.customername,
-      customeraddress: request.body.customeraddress,
-      items: request.body.items,
-      finalprice: request.body.finalprice,
-      terms: request.body.terms,
-      invoicedescription: request.body.invoicedescription,
-    },
-    function (err, result) {
-      if (err) {
-        console.log("ERROR: " + err);
-        response
-          .status(500)
-          .json({ message: "no se pudo actualizar la información" });
-      } else {
-        console.log("la info se ha actualizado correctamente");
-        response
-          .status(200)
-          .json({ message: "la info se ha actualizado correctamente" });
-      }
+  const id = request.params.invoiceId;
+  const input = request.body;
+  var conString =
+    "postgres://tbsifhep:vc-HK00-0KSl1FysW9tMUe7jehXh0Qx4@rogue.db.elephantsql.com/tbsifhep"; //Can be found in the Details page
+  var client = new pg.Client(conString);
+  client.connect(function (err) {
+    if (err) {
+      return console.error("could not connect to postgres", err);
     }
-  );
+    client.query(
+      `UPDATE INVOICE
+      SET sellername = '${input.sellername}',
+      selleraddress = '${input.selleraddress}',
+      customername = '${input.customername}',
+      customeraddress = '${input.customeraddress}',
+      finalprice = '${input.finalprice}',
+      terms = '${input.terms}',
+      invoicedescription = '${input.invoicedescription}'
+      WHERE id = ${id}`,
+      function (err, result) {
+        if (err) {
+          return console.error("error running query", err);
+        }
+        response.status(201).json({});
+        client.end();
+      }
+    );
+  });
 });
 
 module.exports = router;
